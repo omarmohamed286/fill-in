331e2db84ce1fb0adc20e8b144036048dd003dae
Omar Mohamed <125928590+omarmohamed286@users.noreply.github.com>
2025-11-20 21:24:14 +0200

    landing page

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
       </Routes>
     </BrowserRouter>
   );
