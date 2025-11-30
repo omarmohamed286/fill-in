ce6f65eaa1a20e40f0b7d28603f64edcb736cd14
Omar Mohamed <125928590+omarmohamed286@users.noreply.github.com>
2025-11-30 02:50:07 +0200

    env

--- client/src/utils/gemini.ts
+++ client/src/utils/gemini.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 const ai = new GoogleGenAI({
-  apiKey: "AIzaSyB8TP3yqb0AbHrdA8Ioc6gahFhSpMSRKXU",
+  apiKey: import.meta.env.VITE_GEMINI_KEY,
 });
 
 const callGemini = async (prompt: string) => {
